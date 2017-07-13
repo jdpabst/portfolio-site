0cd385716355e1d805821165b3abe20d64ac02ec
Jessica <jdhpabst@gmail.com>
2017-07-13 09:18:27 -0600

    updated index.js

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ var config = require('./config.js');
 const app = module.exports = express();
 
 app.use(bodyParser.json());
-app.use(express.static(__dirname + './../build'))
+app.use(express.static(__dirname + './build'))
 
 app.listen(config.port,console.log("you are now connected on 3000, database should work too"));
