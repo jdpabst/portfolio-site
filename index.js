36c29c71dd891d2e280ed90db89b0da444050411
Jessica <jdhpabst@gmail.com>
2017-07-13 16:52:49 -0600

    working with nodemon

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ var config = require('./config.js');
 const app = module.exports = express();
 
 app.use(bodyParser.json());
-app.use(express.static(__dirname + './build'))
+app.use(express.static(__dirname + '/build'))
 
 app.listen(config.port,console.log("You are now connected."));
