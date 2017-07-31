909ae3439f6a42d84382f47725b9d75b9be355c2
Jessica <jdhpabst@gmail.com>
2017-07-31 12:51:45 -0600

    working on getting media queries to work for projects

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -10,27 +10,15 @@ import './Portfolio.css'
 
 export default class SectionOne extends Component{
 
-// componentDidMount(){
-//     $(document).ready(function(){
-//         $('.projects_holder').slick();
-//     });
-// }
+    constructor(props){
+        super(props);
 
+        this.WidthChange = this.WidthChange.bind(this);
+    }
 
-    render(){
-        let settings = {
-            dots: true,
-            infinite: true,
-            arrows: false,
-            speed: 500,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            autoplay: true,
-            swipe: true,
-            draggable: true,
-
-        }
-        let settings2 = {
+    WidthChange(mq) {
+        if(mq.matches){
+            this.settings = {
             dots: true,
             infinite: true,
             arrows: false,
@@ -38,16 +26,70 @@ export default class SectionOne extends Component{
             slidesToShow: 3,
             slidesToScroll: 3,
             autoplay: true,
+            swipe: true,
+            draggable: true,
+
+            }
+        } else {
+            this.settings = {
+            dots: true,
+            infinite: true,
+            arrows: false,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
             draggable: true,
         }
+
+        }
+    }
+
+
+    render(){
+        const mq = window.matchMedia( "(min-width: 960px)" );
+        this.settings;
+
+        if (matchMedia) {
+            let widthChange = this.WidthChange;
+            const mq = window.matchMedia("(min-width: 960px)");
+            mq.addListener(widthChange);
+            widthChange(mq);
+        }
+
+        if(mq.matches){
+            this.settings = {
+            dots: true,
+            infinite: true,
+            arrows: false,
+            speed: 500,
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            autoplay: true,
+            swipe: true,
+            draggable: true,
+
+            }
+        } else {
+            this.settings = {
+            dots: true,
+            infinite: true,
+            arrows: false,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            draggable: true,
+        }
+    }
         return(
             <section id="portfolio" className="main_portfolio_section">
                 <div id="portfolio_title">
                     <h1>Portfolio</h1>
                     <p>Recent Projects</p>
                 </div>
-                <div id="projects_holder" className="projects_holder">
-                    <Slider {...settings2}>
+                {/* <div id="projects_holder" className="projects_holder">
+                   <Slider {...settings}>
                             <div className="personal_proj">
                                 <div className="personal_hover">
                                     <div id="personal_hover_overlay">
@@ -114,9 +156,9 @@ export default class SectionOne extends Component{
                             </div>
                         </Slider>
                 </div>
-            
+             */}
                 <div id="mobile_projects_holder" className="mobile_projects_holder">
-                    <Slider {...settings}>
+                    <Slider {...this.settings}>
                         <div className="personal_proj_div">
                             <div className="personal_proj">
                                 <div className="personal_hover">
