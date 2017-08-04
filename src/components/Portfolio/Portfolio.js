50931e262befbad477c43340fa2a272a1a6f65f4
Jessica <jdhpabst@gmail.com>
2017-08-04 15:11:15 -0600

    added todo

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -83,75 +83,6 @@ export default class SectionOne extends Component{
                     <h1>Portfolio</h1>
                     <p>Recent Projects</p>
                 </div>
-                {/* <div id="projects_holder" className="projects_holder">
-                   <Slider {...settings}>
-                            <div className="personal_proj">
-                                <div className="personal_hover">
-                                    <div id="personal_hover_overlay">
-                                    </div>
-                                    <div className="personal_hover_words">
-                                        <h1>Jamba Juice</h1>
-                                        <p>Clone</p>
-                                        <p>Angular</p>
-                                        <p>Node</p>
-                                        <p>PostgresSQL</p>
-                                    </div>
-                                    <div className="personal_hover_links">
-                                        <h1><a href="https://youtu.be/-BnIAlkpM6E" target="_blank">Walk-Through</a></h1>
-                                        <h2><a href="http://jjclone.jdpabst.com" target="_blank">Live Site</a></h2>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="group_proj">
-                                <div className="group_hover">
-                                    <div id="group_hover_overlay">
-                                    </div>
-                                    <div className="group_hover_words">
-                                        <h1>YouTube</h1>
-                                        <p>Clone</p>
-                                        <p>React</p>
-                                        <p>Node</p>
-                                        <p>PostgresSQL</p>
-                                    </div>
-                                    <div className="group_hover_links">
-                                        <h1><a href="https://youtu.be/3fRNWnwc49o" target="_blank">Walk-Through</a></h1>
-                                        <h2><a href="http://ytclone.jdpabst.com" target="_blank">Live Site</a></h2>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="no_server">
-                                <div className="no_server_hover">
-                                    <div id="no_server_hover_overlay">
-                                    </div>
-                                    <div className="no_server_hover_words">
-                                        <h1>Bucket List</h1>
-                                        <p>No-Server App</p>
-                                        <p>Angular</p>
-                                    </div>
-                                    <div className="no_server_hover_links">
-                                        <h1><a href="https://youtu.be/ymB0DqQWd2c" target="_blank">Walk-Through</a></h1>
-                                        <h2><a href="https://jdpabst.github.io/#!/" target="_blank">Live Site</a></h2>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="zodiac">
-                                <div className="zodiac_hover">
-                                    <div id="zodiac_hover_overlay">
-                                    </div>
-                                    <div className="zodiac_hover_words">
-                                        <h1>Bucket List</h1>
-                                        <p>No-Server App</p>
-                                        <p>Angular</p>
-                                    </div>
-                                    <div className="zodiac_hover_links">
-                                        <h1><a href="" target="_blank">Walk-Through</a></h1>
-                                        <h2><a href="http://empyrean.jdpabst.com/#!/" target="_blank">Live Site</a></h2>
-                                    </div>
-                                </div>
-                            </div>
-                        </Slider>
-                </div>
-             */}
                 <div id="mobile_projects_holder" className="mobile_projects_holder">
                     <Slider {...this.settings}>
                         <div className="personal_proj_div">
@@ -247,41 +178,6 @@ export default class SectionOne extends Component{
                         </div>
                     </Slider>
                 </div>
-
-                {/*<div id="personal_holder">
-                    <div className="personal_proj_info">
-                        <h2>Jamba Juice</h2>
-                        <p>Clone</p>
-                        <p>Angular</p>
-                        <p>Node</p>
-                        <p>PostgresSQL</p>
-                    </div>
-                </div>
-                <div className="personal_proj_img">
-                        <img src={ jamba }/>
-                </div>
-                <div id="group_holder">
-                    <div className="group_proj_info">
-                        <h2>YouTube</h2>
-                        <p>Clone</p>
-                        <p>React</p>
-                        <p>Node</p>
-                        <p>PostgresSQL</p>
-                    </div>
-                </div>
-                <div className="group_proj_img">
-                        <img src={ youtube }/>
-                </div>
-                <div id="no_server_holder">
-                    <div className="no_server_proj_info">
-                        <h2>Bucket List App</h2>
-                        <p>No-Server</p>
-                        <p>Angular</p>
-                    </div>
-                </div>
-                <div className="no_server_proj_img">
-                        <img src={ youtube }/>
-                </div>*/}
             </section>
         )
     }
